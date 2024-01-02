8f952674ec8ea706636ea6c1c0575d65943ecb9e
Adi Ardiansyah <59949473+JonesqPacMan@users.noreply.github.com>
2024-01-02 22:40:41 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -82,17 +82,13 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.keymaster@4.1 \
     android.hardware.vibrator-V1-ndk_platform \
     android.hardware.graphics.common@1.0 \
-    libtinyalsa \
-    libion \
-    libxml2
+    libion
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.graphics.common@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libtinyalsa.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
 
 # Bootctrl
 PRODUCT_PACKAGES += \
